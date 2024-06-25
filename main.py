c2d8f5ae61077ad7c9585a7a2ff1e247caedf71a
Habib <mosaddikhabib.bsfmstu@gmail.com>
2024-06-25 16:20:04 +0600

    placeholder added in user and password field

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 import tkinter as tk
 from tkinter import ttk, CENTER, TOP, LEFT
-from tkinter.messagebox import askyesno
 from PIL import Image, ImageTk
+
+class PageOne(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
+        self.configure(bg='#222D20')
+
+        label = tk.Label(self, text="Page One", font=("Helvetica", 16), bg='#222D20', fg='white')
+        label.pack(side="top", fill="x", pady=10)
+
+        button = ttk.Button(self, text="Go to the Start Page",
+                            command=lambda: controller.show_frame("StartPage"))
+        button.pack(pady=5)
+
+class PageTwo(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
+        self.configure(bg='#A22F20')
+
+        label = tk.Label(self, text="Page Two", font=("Helvetica", 16))
+        label.pack()
+
+        button = ttk.Button(self, text="Go to the Start Page",
+                            command=lambda: controller.show_frame("StartPage"))
+        button.pack(pady=5)
 
 class MainApp(tk.Tk):
     def __init__(self, *args, **kwargs):
@@ -9,8 +34,6 @@ class MainApp(tk.Tk):
 
         # Set the window size and title
         self.geometry("1920x1080")
-        # self.geometry("600x450")
-
         self.title("hostMate | RajIT | made for RMCH")
 
         # Create a container for all the pages
@@ -56,10 +79,40 @@ class StartPage(tk.Frame):
         input_text_username = tk.StringVar()
         userName = tk.Entry(self, width=30, textvariable=input_text_username, font=("Inter", 12), justify=LEFT, bg="#D9D9D9", fg="black")
         userName.pack(side=TOP, pady=10, ipady=5)
+        userName.insert(0, "User name")
 
         input_text_password = tk.StringVar()
         userPass = tk.Entry(self, width=30, textvariable=input_text_password, show="*", font=("Inter", 12), justify=LEFT, bg="#D9D9D9", fg="black")
         userPass.pack(side=TOP, pady=10, ipady=5)
+        userPass.insert(0, "Password")
+
+        def on_enter_username(event):
+            if userName.get() == "User name":
+                userName.delete(0, "end")
+                userName.config(fg="black")
+
+        def on_leave_username(event):
+            if userName.get() == "":
+                userName.insert(0, "User name")
+                userName.config(fg="gray")
+
+        def on_enter_password(event):
+            if userPass.get() == "Password":
+                userPass.delete(0, "end")
+                userPass.config(fg="black", show="*")
+
+        def on_leave_password(event):
+            if userPass.get() == "":
+                userPass.insert(0, "Password")
+                userPass.config(fg="gray", show="")
+
+        userName.bind("<FocusIn>", on_enter_username)
+        userName.bind("<FocusOut>", on_leave_username)
+        userName.config(fg="gray")
+
+        userPass.bind("<FocusIn>", on_enter_password)
+        userPass.bind("<FocusOut>", on_leave_password)
+        userPass.config(fg="gray", show="")
 
         def login():
             username = input_text_username.get()
@@ -78,36 +131,6 @@ class StartPage(tk.Frame):
         # Temp button
         button2 = tk.Button(self, text="Go to Page Two", command=lambda: controller.show_frame("PageTwo"), font=("Inter", 12, "bold"), bg='#AD9309', fg='white')
         button2.pack(pady=5)
-
-
-class PageOne(tk.Frame):
-    def __init__(self, parent, controller):
-        super().__init__(parent)
-        self.controller = controller
-        self.configure(bg='#222D20')
-
-        label = tk.Label(self, text="Page One", font=("Helvetica", 16), bg='#222D20', fg='white')
-        label.pack(side="top", fill="x", pady=10)
-
-        button = ttk.Button(self, text="Go to the Start Page",
-                            command=lambda: controller.show_frame("StartPage"))
-        button.pack(pady=5)
-
-class PageTwo(tk.Frame):
-    def __init__(self, parent, controller):
-        super().__init__(parent)
-        self.controller = controller
-        self.configure(bg='#A22F20')
-
-        # label = tk.Label(self, text="Page Two", font=("Helvetica", 16), bg='#222D20', fg='white')
-        label = tk.Label(self, text="Page Two", font=("Helvetica", 16))
-        label.pack()
-        # label.pack(side="top", fill="x", pady=10)
-
-
-        button = ttk.Button(self, text="Go to the Start Page",
-                            command=lambda: controller.show_frame("StartPage"))
-        button.pack(pady=5)
 
 if __name__ == "__main__":
     app = MainApp()
